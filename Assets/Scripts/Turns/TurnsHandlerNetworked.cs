f8c55d4e663c6d91e395aaf50a8714ad060e9d4a
Harry <henry.iz@yandex.ru>
2024-03-09 00:14:22 +0300

    Запуск первого игрового раунда

--- Assets/Scripts/Turns/TurnsHandlerNetworked.cs
+++ Assets/Scripts/Turns/TurnsHandlerNetworked.cs
@@ -6,6 +6,16 @@ using UnityEngine;
 
 public class TurnsHandlerNetworked : TurnsHandler
 {
+    public override void OnStartServer()
+    {
+        PlayerPiecesHandler.OnPiecesSpawned += NextTurn;
+        Players = LocalGameManager.Instance.Players;
+    }
+
+    public override void OnStopServer()
+    {
+        PlayerPiecesHandler.OnPiecesSpawned -= NextTurn;
+    }
     protected override void FillMovesList()
     {
         base.FillMovesList();
